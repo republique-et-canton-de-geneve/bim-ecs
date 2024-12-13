fc155aaee52bae783f7eae9631aa1e7bc0e12d0a
Charles HETIER <charles.hetier@etat.ge.ch>
2024-12-13 11:12:00 +0100

    addition resources module exposed

--- packages/ecs/src/resources/index.ts
+++ packages/ecs/src/resources/index.ts
@@ -1,3 +1,4 @@
 export { EcsResource } from './ecs-resource';
 export { EcsResourceFactory } from './ecs-resource-factory';
 export type { EcsResourceKey } from './ecs-resource-key';
+export type { ExtractEcsResourceKeyType } from './extract-ecs-resource-key-type';

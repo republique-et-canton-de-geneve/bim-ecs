f0f7dd0262dbea55068f3a97abda45e998d59446
Charles HETIER <charles.hetier@etat.ge.ch>
2025-02-26 15:19:06 +0100

    SystemSpecification exposed

--- packages/ecs/src/systems/index.ts
+++ packages/ecs/src/systems/index.ts
@@ -2,4 +2,5 @@ export { defineSystem } from './define-system';
 export { SystemRunner } from './system-runner';
 export { defineMixin } from './define-mixin';
 export type { SystemDefinition } from './system-definition';
+export type { SystemSpecification } from './system-specification';
 export * from './commands';
